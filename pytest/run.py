ec1cc0245d4c4cff908c2e6e74d291426ae82147
Yuval Shavit <yankee.sierra@gmail.com>
2017-03-28 23:27:14 -0400

    use stream for denter (and parser)

--- pytest/run.py
+++ pytest/run.py
@@ -27,6 +27,7 @@ def find_results(tests_dir=TESTS_DIR):
     a list of tuples: (description, expected, actual)'''
     tests_dir_abs = abs_path(tests_dir)
     results = {}
+
     prefix_len = len(tests_dir_abs) + 1  # +1 for the trailing slash
     for test_p in glob.glob('%s**/*.yaml' % tests_dir_abs):
         test_name = test_p[prefix_len:]
@@ -41,12 +42,12 @@ def find_results(tests_dir=TESTS_DIR):
                 entry_module = case['entry_module']
                 sys.stdout.write('  %s:' % desc)
                 sys.stdout.flush()
-                actual = run_effes(entry_module, stdin)
+                actual, actual_err = run_effes(entry_module, stdin)
                 if stdout == actual:
-                    print ' OK'
+                    print ' OK%s' % (', but with stderr' if actual_err else '')
                 else:
                     print ' \x1b[1mFAIL\x1b[0m'
-                    case_results.append((desc, stdout, actual))
+                case_results.append((desc, stdout, actual, actual_err))
             results[test_name] = case_results
     return results
 
@@ -56,10 +57,9 @@ def run_effes(entry_module, stdin):
     proc_args = ['java', '-Ddebug=%s' % debug_option, '-jar', abs_path(EF_JAR), entry_module]
     classpath = abs_path(EF_CLASSPATH)
     proc_env = {'EFFES_CLASSPATH': classpath}
-    p = subprocess.Popen(proc_args, stdin=PIPE, stdout=PIPE, env=proc_env)
-    stdout, ignored = p.communicate(stdin)
-    out = stdout.strip()
-    return out
+    p = subprocess.Popen(proc_args, stderr=PIPE, stdin=PIPE, stdout=PIPE, env=proc_env)
+    stdout, stderr = p.communicate(stdin)
+    return stdout.strip(), stderr.strip()
 
 
 def clear_dir(dir_p):
@@ -74,12 +74,18 @@ def results_to_files(results):
     for test_p, case_results in results.iteritems():
         expected_d = clear_dir('%s/%s' % (EXPECTEDS_DIR, test_p))
         actual_d = clear_dir('%s/%s' % (ACTUALS_DIR, test_p))
-        for desc, expected, actual in case_results:
+        for desc, expected, actual, actual_err in case_results:
+            if expected == actual and (not actual_err):
+                continue
             desc = re.sub('\\W', '_', desc)
             with open('%s/%s' % (expected_d, desc), 'w') as expect_f, \
                  open('%s/%s' % (actual_d, desc), 'w') as actual_f:
-                expect_f.write(expected)
-                actual_f.write(actual)
+                if expected != actual:
+                    expect_f.write(expected)
+                    actual_f.write(actual)
+                if actual_err:
+                    actual_f.write('%s\nstder:\n' % ('-' * 20))
+                    actual_f.write(actual_err)
 
 
 if __name__ == '__main__':
@@ -92,10 +98,14 @@ if __name__ == '__main__':
         exit(50)
     results = find_results()
     results_to_files(results)
+    # results is a map of test -> [(desc, expected, actual, actual_err)]
+    errs = []
+    for file_results in results.itervalues():
+        errs.extend([e for e in file_results if e[1] != e[2]])
 
-    case_errors = reduce(lambda a, b: a + b, map(len, results.itervalues()))
+    case_errors = reduce(lambda a, b: a + b, map(len, errs), 0)
     if case_errors:
-        test_errors = len(filter(bool, results.itervalues()))
+        test_errors = len(filter(bool, errs))
         print
         print '%d error%s in %d test file%s.' % (
                 case_errors,
