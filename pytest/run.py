a35580c0d6d8ab06fba29b32f607603b85fa3b6b
Yuval Shavit <yankee.sierra@gmail.com>
2017-04-26 01:49:13 -0400

    test framework can read/write files
    also include a compiler test that uses that functionality

--- pytest/run.py
+++ pytest/run.py
@@ -6,6 +6,7 @@ import shutil
 import subprocess
 from subprocess import PIPE
 import sys
+import tempfile
 import time
 import yaml
 
@@ -52,23 +53,31 @@ def find_results(desc_filter=None, tests_dir=TESTS_DIR):
                     sys.stdout.write(' %s:' % desc)
                     sys.stdout.flush()
                     entry_module = case['entry_module']
-                    actual, actual_err = run_effes(entry_module, stdin)
-                    if stdout == actual:
+                    args = case.get('args', [])
+                    in_files = case.get('initial_files', {})
+                    actual, actual_err, actual_files = run_effes(entry_module, stdin, args, in_files)
+                    expected_files = case.get('result_files', {})
+                    expected_files = fill_in_file_shorthand(expected_files, in_files)
+                    if stdout == actual and expected_files == actual_files:
                         print ' OK%s' % (', but with stderr' if actual_err else '')
                     else:
                         print ' \x1b[1mFAIL\x1b[0m'  # bold
-                    case_results.append((desc, stdout, actual, actual_err))
+                    case_results.append((desc, stdout, actual, actual_err,
+                                         expected_files, actual_files))
             if case_results:
                 results[test_name] = case_results
     return results
 
 
-def run_effes(entry_module, stdin):
+def run_effes(entry_module, stdin, args, in_files):
     debug_option = os.environ.get(DEBUG_KEY, '')
     proc_args = ['java', '-Ddebug=%s' % debug_option, '-jar', abs_path(EF_JAR), entry_module]
+    proc_args.extend(args)
     classpath = abs_path(EF_CLASSPATH)
     proc_env = {'EFFES_CLASSPATH': classpath}
-    p = subprocess.Popen(proc_args, stderr=PIPE, stdin=PIPE, stdout=PIPE, env=proc_env)
+    working_dir = tempfile.mkdtemp()
+    create_files(working_dir, in_files)
+    p = subprocess.Popen(proc_args, stderr=PIPE, stdin=PIPE, stdout=PIPE, env=proc_env, cwd=working_dir)
     if debug_option.lower() == '0:suspend':
         sys.stderr.write(' (pid %s' % p.pid)
         sys.stderr.flush()
@@ -87,9 +96,51 @@ def run_effes(entry_module, stdin):
         sys.stderr.write(')')
         sys.stderr.flush()
     stdout, stderr = p.communicate(stdin)
-    return stdout.strip(), stderr.strip()
+    out_files = dir_to_object(working_dir)
+    shutil.rmtree(working_dir)
+    return stdout.strip(), stderr.strip(), out_files
 
 
+def create_files(cwd, files):
+    for name, contents in files.iteritems():
+        child_path = '%s/%s' % (cwd, name)
+        if type(contents) is dict:
+            os.mkdir(child_path)
+            create_files(child_path, contents)
+        elif type(contents) is str:
+            with open(child_path, 'w') as fh:
+                fh.write(contents)
+        else:
+            raise Exception('unknown type for %s: %s (%s)' % (
+                name, type(contents), contents))
+
+
+def dir_to_object(cwd):
+    obj = {}
+    for name in os.listdir(cwd):
+        path = '%s/%s' % (cwd, name)
+        if os.path.isdir(path):
+            obj[name] = dir_to_object(path)
+        else:
+            with open(path) as fh:
+                obj[name] = fh.read()
+    return obj
+
+
+def fill_in_file_shorthand(shorthand, template):
+    '''for each (k, v) in shorthand, if the value is ['...'], then use
+    the value from template instead.'''
+    for k, v in shorthand.iteritems():
+        if v == ['...']:
+            if k in template:
+                shorthand[k] = template[k]
+            else:
+                del shorthand[k]
+        elif type(v) is not str:
+            raise Exception('no recursion yet')
+    return shorthand
+
+     
 def clear_dir(dir_p):
     dir_p = abs_path(dir_p)
     if os.path.exists(dir_p):
@@ -99,20 +150,38 @@ def clear_dir(dir_p):
 
 
 def results_to_files(results):
+    def line_break(fh, header):
+        fh.write('%s\n%s:\n' % ('-' * 20, header))
+
     for test_p, case_results in results.iteritems():
         expected_d = clear_dir('%s/%s' % (EXPECTEDS_DIR, test_p))
         actual_d = clear_dir('%s/%s' % (ACTUALS_DIR, test_p))
-        for desc, expected, actual, actual_err in case_results:
-            if expected == actual and (not actual_err):
+        for r in case_results:
+            (
+             desc,
+             expected,
+             actual,
+             actual_err, 
+             expected_files, 
+             actual_files
+            ) = r
+            matched_stdout = (expected == actual)
+            matched_files = (expected_files == actual_files)
+            if matched_stdout and matched_files and (not actual_err):
                 continue
             desc = re.sub('\\W', '_', desc)
             with open('%s/%s' % (expected_d, desc), 'w') as expect_f, \
                  open('%s/%s' % (actual_d, desc), 'w') as actual_f:
-                if expected != actual:
+                if not matched_stdout:
                     expect_f.write(expected)
                     actual_f.write(actual)
+                if not matched_files:
+                    line_break(expect_f, 'files')
+                    expect_f.write(yaml.dump(expected_files, default_style='|'))
+                    line_break(actual_f, 'files')
+                    actual_f.write(yaml.dump(actual_files, default_style='|'))
                 if actual_err:
-                    actual_f.write('%s\nstder:\n' % ('-' * 20))
+                    line_break(actual_f, 'stderr')
                     actual_f.write(actual_err)
 
 
@@ -132,8 +201,8 @@ if __name__ == '__main__':
     results_to_files(results)
     # results is a map of test -> [(desc, expected, actual, actual_err)]
     errs = []
-    for file_results in results.itervalues():
-        errs.extend([e for e in file_results if e[1] != e[2]])
+    for file_res in results.itervalues():
+        errs.extend(['e' for e in file_res if e[1] != e[2] or e[4] != e[5]])
 
     case_errors = reduce(lambda a, b: a + b, map(len, errs), 0)
     if case_errors:
