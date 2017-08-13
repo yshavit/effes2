378b9fa148f3f19985b47d63817710cbd39aae69
Yuval Shavit <yankee.sierra@gmail.com>
2017-08-12 23:40:56 -0400

    more test refactoring

--- effes2j/src/test/java/com/yuvalshavit/effes2/parse/test/ParseTest.java
+++ effes2j/src/test/java/com/yuvalshavit/effes2/parse/test/ParseTest.java
@@ -4,49 +4,40 @@ import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertNotNull;
 
 import java.io.IOException;
+import java.util.function.Consumer;
 import java.util.function.Function;
 
 import org.antlr.v4.runtime.ParserRuleContext;
-import org.antlr.v4.runtime.tree.Tree;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import com.yuvalshavit.effes2.parse.EffesParser;
 import com.yuvalshavit.effes2.parse.ParseUtils;
 import com.yuvalshavit.effes2.parse.ToObjectPrinter;
+import com.yuvalshavit.effes2.util.ParseChecker;
 import com.yuvalshavit.effes2.util.ResourceReader;
 
 public class ParseTest {
-  public static final String PARSE_TESTS = "test1";
 
   @Test
   public void dataProviderWorks() throws IOException {
     assertNotNull(readParseFiles());
   }
 
-  @DataProvider(name = PARSE_TESTS)
+  @DataProvider(name = "test1")
   public static Object[][] readParseFiles() throws IOException {
     return ResourceReader.testCases(ParseTest.class, TestCase.class);
   }
 
-  @Test(dataProvider = PARSE_TESTS)
+  @Test(dataProvider = "test1")
   public void parse(String fileName, TestCase testCase) throws Exception {
     Function<EffesParser,ParserRuleContext> rule = ParseUtils.ruleByName(fileName);
-    StringBuilder errsSb = new StringBuilder();
-    Tree ast = ParseUtils.parse(
-      testCase.input,
-      rule,
-      ((line, lineOffset, msg) -> errsSb.append(String.format("%s error at <%s> %d:%d: %s%n", getClass().getSimpleName(), testCase, line, lineOffset, msg))));
-    ToObjectPrinter toObjectPrinter = new ToObjectPrinter();
-    toObjectPrinter.walk(ast);
-    Object result = toObjectPrinter.get();
-
-    String errMessages = errsSb.toString();
-    if (!errMessages.isEmpty()) {
-      System.err.println(errMessages); // we'll assertEquals on them later, but for now print them in case the prettyPrint fails first.
-    }
-    assertEquals(ParseUtils.prettyPrint(result), ParseUtils.prettyPrint(testCase.expected));
-    assertEquals(errMessages, "", "error messages");
+    ParseChecker.check(getClass().getSimpleName() + "." + testCase, testCase.input, rule, ast -> {
+      ToObjectPrinter toObjectPrinter = new ToObjectPrinter();
+      toObjectPrinter.walk(ast);
+      Object result = toObjectPrinter.get();
+      assertEquals(ParseUtils.prettyPrint(result), ParseUtils.prettyPrint(testCase.expected));
+    });
   }
 
   static class TestCase {
---
+++ effes2j/src/test/java/com/yuvalshavit/effes2/util/ParseChecker.java
@@ -0,0 +1,30 @@
+package com.yuvalshavit.effes2.util;
+
+import static org.testng.Assert.assertEquals;
+
+import java.util.function.Consumer;
+import java.util.function.Function;
+
+import org.antlr.v4.runtime.ParserRuleContext;
+
+import com.yuvalshavit.effes2.parse.EffesParser;
+import com.yuvalshavit.effes2.parse.ParseUtils;
+
+public class ParseChecker {
+  private ParseChecker() {}
+
+  public static <R extends ParserRuleContext> void check(String description, String input, Function<EffesParser,R> rule, Consumer<R> astChecker) {
+    StringBuilder errsSb = new StringBuilder();
+    R ast = ParseUtils.parse(
+      input,
+      rule,
+      ((line, lineOffset, msg) -> errsSb.append(String.format("error at <%s> %d:%d: %s%n", description, line, lineOffset, msg))));
+
+    String errMessages = errsSb.toString();
+    if (!errMessages.isEmpty()) {
+      System.err.println(errMessages); // we'll assertEquals on them later, but for now print them in case the prettyPrint fails first.
+    }
+    astChecker.accept(ast);
+    assertEquals(errMessages, "", "error messages");
+  }
+}
