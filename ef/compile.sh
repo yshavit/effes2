cfdc09a06414ebed9c24cc1bbb19ada6632670d2
Yuval Shavit <yankee.sierra@gmail.com>
2017-12-24 03:52:50 -0500

    fix compile.ef, add debug opts

--- ef/compile.sh
+++ ef/compile.sh
@@ -1,5 +1,9 @@
-#!/usr/bin/bash
+#!/bin/bash
 
 cd "$(dirname $0)"
 compiler_jar=../effes2j/target/effes2j-1.0-SNAPSHOT-jar-with-dependencies.jar
-java -Dsource=. -Doutput=../efct -jar "$compiler_jar" 
+DEBUG_OPTS=''
+if [[ -n "$EF_DEBUG" ]]; then
+  DEBUG_OPTS='-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005'
+fi
+java $DEBUG_OPTS -Dsource=. -Doutput=../efct -jar "$compiler_jar" 
