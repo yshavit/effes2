d2b27f9dcf5e29a8625e6273167aed29ee2c9f59
Yuval Shavit <yankee.sierra@gmail.com>
2018-06-17 02:57:19 -0400

    add option for running with code coverage
    see yshavit/effesvm_j@aecfd9c5

--- ef/run.sh
+++ ef/run.sh
@@ -10,9 +10,12 @@ export EFFES_CLASSPATH=../efct
 if [[ -n "$EVM_DEBUG" ]]; then
   EVM_DEBUG="-Ddebug=$EVM_DEBUG"
 fi
+if [[ -n "$COVERAGE" ]]; then
+  COVERAGE="-Dcoverage=$COVERAGE"
+fi
 if [[ -n "${YOURKIT+x}" && -n "$YOURKIT_AGENT" ]]; then
   # https://www.yourkit.com/docs/java/help/startup_options.jsp
   YOURKIT_OPTS="dir=yourkit_profiles,${YOURKIT:-sampling}"
   YOURKIT_AGENT_OPTS="-agentpath:$YOURKIT_AGENT=$YOURKIT_OPTS"
 fi
-java $EF_DEBUG $EVM_DEBUG $YOURKIT_AGENT_OPTS -jar "$effes_jar" "$@"
+java $EF_DEBUG $EVM_DEBUG $YOURKIT_AGENT_OPTS $COVERAGE -jar "$effes_jar" "$@"
